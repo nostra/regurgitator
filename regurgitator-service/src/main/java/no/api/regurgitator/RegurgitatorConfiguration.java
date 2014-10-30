a31455d77865b0b14da79a2c6c83fc5ef366fc93
Erlend Nossum <erlend@api.no>
2014-10-30 08:56:50 +0100

    Added ignoreUnknown

--- regurgitator-service/src/main/java/no/api/regurgitator/RegurgitatorConfiguration.java
+++ regurgitator-service/src/main/java/no/api/regurgitator/RegurgitatorConfiguration.java
@@ -1,8 +1,8 @@
 package no.api.regurgitator;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
-import org.apache.commons.lang.StringUtils;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -10,6 +10,7 @@ import javax.validation.constraints.NotNull;
 /**
  *
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RegurgitatorConfiguration extends Configuration {
     @Valid
     @NotNull
